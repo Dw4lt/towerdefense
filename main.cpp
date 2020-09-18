2e7f33754ee1050bbcd00c7a6dc8b4b91bc233bd
Dw4lt <142900824+Dw4lt@users.noreply.github.com>
2020-09-18 17:05:33 +0200

    Increase field size

--- main.cpp
+++ main.cpp
@@ -18,21 +18,20 @@ int main ()
 
     Renderer* renderer = Renderer::Init(320, 240, 16);
     
-    Field* field = new Field(0,0,320,210,20,15);
-    Enemy* enemy = new Enemy(field->get(field->getStart())->getCenter(), 6, 6, Point{field->getStart()}, 100, 4.2, 0xf00f);
+    Field* field = new Field(0,0,320,210,40,30);
+    Enemy* enemy = new Enemy(field->get(field->getStart())->getCenter(), 4, 4, Point{field->getStart()}, 100, 4.2, 0xf00f);
     renderer->addToSchene(field,0);
     renderer->addToSchene(enemy, 2);
-    enemy->pathfind(field);
 
-    for (int i = 0; i < 120; i++){
+    for (int i = 0; i < 240; i++){
         enemy->pathfind(field);
         Uint32 start = SDL_GetTicks();
         renderer->render();
         renderer->display();
         Uint32 stop = SDL_GetTicks();
         Uint32 tick_duration = stop - start;
-        if (tick_duration < 160){
-            SDL_Delay(16-tick_duration);
+        if (tick_duration < 41){
+            SDL_Delay(41-tick_duration);
         }
     }
     //SDL_UpdateRect(screen,0,0,50,50);
