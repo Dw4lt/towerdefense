a14bd14f7f039706dd721dfaea87b724efd63e41
Dw4lt <142900824+Dw4lt@users.noreply.github.com>
2023-09-05 09:35:51 +0000

    Replace macros with constexpr functions

--- src/primitives/color_conversion.hpp
+++ src/primitives/color_conversion.hpp
@@ -1,9 +1,16 @@
 #ifndef COLOR_CONVERSION
 #define COLOR_CONVERSION
 
-#define RGB_888_TO_565(x) ((x & 0xF8) >> 3) + ((x & 0xfC00) >> 5) + ((x & 0xf80000) >> 8)
-#define RGB_565_TO_888(x) ((x & 0x3F) << 3) + ((x & 0x7f00) << 5) + ((x & 0x3f0000) << 8)
+constexpr int RGB_888_TO_565(int x) {
+    return ((x & 0xF8) >> 3) + ((x & 0xfC00) >> 5) + ((x & 0xf80000) >> 8);
+}
 
-#define PSEUDO_RANDOM_SEEDED(x, y) (((x * 5 + y * 7 + 351) * 795) % (x * y + 3 * (x + y))) // Gives back a random looking number based on the coords
+constexpr int RGB_565_TO_888(int x) {
+    return ((x & 0x3F) << 3) + ((x & 0x7f00) << 5) + ((x & 0x3f0000) << 8);
+}
+
+constexpr int PSEUDO_RANDOM_SEEDED(int x, int y) {  // Gives back a pseudo-random looking number based on the coords
+    return (((x * 5 + y * 7 + 351) * 795) % (x * y + 3 * (x + y)));
+}
 
 #endif
