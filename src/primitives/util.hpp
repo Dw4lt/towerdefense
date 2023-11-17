84a694a439627f20d5c1e116b2bee71c36f98918
Dw4lt <142900824+Dw4lt@users.noreply.github.com>
2023-11-17 19:00:05 +0000

    Fix logging line output

--- src/primitives/util.hpp
+++ src/primitives/util.hpp
@@ -5,13 +5,15 @@
 #ifdef DEBUG
     #include <cstdio>
     #include <cstdarg>
-    inline void LOG(const char* format, ...) {
+    inline void LOG_(const char* file, const char* func, int line, const char* format, ...) {
         va_list args;
         va_start(args, format);
-        printf("[%s:%s:%d]: ", __FILE__, __func__, __LINE__);
+        printf("[%s:%s:%d]: ", file, func, line);
         vprintf(format, args);
         va_end(args);
     }
+
+    #define LOG(args...) LOG_(__FILE__, __func__, __LINE__, args)
 #else
     inline void LOG(...) {}
 #endif
