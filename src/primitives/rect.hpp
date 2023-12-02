78d71c6c333b364c28f409fe3f41e166b0b1e9cd
Dw4lt <142900824+Dw4lt@users.noreply.github.com>
2023-12-02 13:27:16 +0000

    Minor improvements to RectTemplate

--- src/primitives/rect.hpp
+++ src/primitives/rect.hpp
@@ -13,15 +13,27 @@ public:
     RectTemplate(T x = 0, T y = 0, U width = 1, U height = 1)
         : origin_{x, y}, width_{width}, height_{height} {};
 
-    RectTemplate<T, U> centeredOn(const PointTemplate<T>& point) const {
-        return centeredOn(point, width_, height_);
+    RectTemplate& centerOn(const PointTemplate<T>& point) noexcept {
+        return centerOn(point.x_, point.y_);
     };
 
-    static RectTemplate<T, U> centeredOn(const PointTemplate<T>& point, int width, int height) {
+    RectTemplate& centerOn(T x, T y) noexcept {
+        origin_.x_ = x - width_ / 2;
+        origin_.y_ = y - height_ / 2;
+        return *this;
+    };
+
+    RectTemplate& resize(U delta_width, U delta_height) noexcept {
+        width_ += delta_width;
+        height_ += delta_height;
+        return *this;
+    };
+
+    static RectTemplate<T, U> centeredOn(const PointTemplate<T>& point, U width, U height) {
         return centeredOn(point.x_, point.y_, width, height);
     };
 
-    static RectTemplate<T, U> centeredOn(T x, T y, int width, int height) {
+    static RectTemplate<T, U> centeredOn(T x, T y, U width, U height) {
         return RectTemplate<T, U>(x - width / 2, y - height / 2, width, height);
     };
 
@@ -29,10 +41,10 @@ public:
         return SDL_Rect{(Sint16)origin_.x_, (Sint16)origin_.y_, (Uint16)width_, (Uint16)height_};
     };
 
-    int right() const { return origin_.x_ + width_ - 1; }; // -1 because a rect of size 1 equates to 1 pixel, the origin pixel
-    int top() const { return origin_.y_; };
-    int left() const { return origin_.x_; };
-    int bottom() const { return origin_.y_ + height_ - 1; }; // -1 because a rect of size 1 equates to 1 pixel, the origin pixel
+    auto right() const { return origin_.x_ + width_ - 1; }; // -1 because a rect of size 1 equates to 1 pixel, the origin pixel
+    T top() const { return origin_.y_; };
+    T left() const { return origin_.x_; };
+    auto bottom() const { return origin_.y_ + height_ - 1; }; // -1 because a rect of size 1 equates to 1 pixel, the origin pixel
 
     PointTemplate<T> center() const {
         return PointTemplate<T>(
